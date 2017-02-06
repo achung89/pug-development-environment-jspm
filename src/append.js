fc3b5743f0fcb362f74ab98f812322eec6951162
Andrew <chung.andrew7@gmail.com>
2017-02-06 00:14:45 -0800

    First commit. Removed dependencies from previous work station.

--- src/append.js
+++ src/append.js
@@ -1,6 +1,8 @@
-import html from './index.pug';
-import '!style-loader!css-loader!sass-loader!./index.scss';
+import html from './index.jade!';
+import '/index.scss!';
 
 var div = document.createElement('div');
 div.innerHTML = html;
 document.body.appendChild(div);
+
+System.import('./script.js');
